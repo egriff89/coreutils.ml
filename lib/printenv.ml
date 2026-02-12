aa04e82e3f1f182c7cfb8d73edef10e0db2edb65
Eric Griffith <griffith.eric@pm.me>
2026-02-11 19:21:05 -0600

    chore: replace `unix` with `core_unix`

--- lib/printenv.ml
+++ lib/printenv.ml
@@ -1,7 +1,7 @@
 let print_null str = Printf.printf "%s%c" str '\000'
 
 let print_env null =
-  let env = Unix.environment () in
+  let env = Core_unix.environment () in
   if null
   then Array.iter (fun x -> print_null x) env
   else Array.iter (fun x -> Printf.printf "%s\n" x) env
