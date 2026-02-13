f2576514dc3e05bb98bea4bbd47fbb7280641bbd
Eric Griffith <griffith.eric@pm.me>
2026-02-13 12:13:57 -0600

    chore(whoami): add Clap description

--- bin/whoami.ml
+++ bin/whoami.ml
@@ -1,6 +1,9 @@
 (*** whoami - print effective user name *)
 
 let () =
+  Clap.description
+    "Print the user name associated with the current effective user ID. Same as id -un.";
+  Clap.close ();
   let username = Core_unix.username () in
   Printf.printf "%s\n" username
 ;;
